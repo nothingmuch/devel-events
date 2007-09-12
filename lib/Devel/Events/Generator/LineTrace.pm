77012b82e5a5fe4e26d4444c359bf6257101a4dc
Yuval Kogman <nothingmuch@woobling.org>
2007-09-12 23:57:58 +0000

    doc Devel::Events::Generator::LineTrace

--- lib/Devel/Events/Generator/LineTrace.pm
+++ lib/Devel/Events/Generator/LineTrace.pm
@@ -37,3 +37,58 @@ sub line {
 __PACKAGE__;
 
 __END__
+
+=pod
+
+=head1 NAME
+
+Devel::Events::Generator::LineTrace - generate C<executing_line> events using
+the perl debugger api.
+
+=head1 SYNOPSIS
+
+	my $g = Devel::Events::Generator::LineTrace->new( handler => $h );
+
+	$g->enable();
+
+	# every line of code will fire an event until
+
+	$g->disable();
+
+=head1 DESCRIPTION
+
+This L<Devel::Events> generator will fire line tracing events using C<DB::DB>,
+a perl debugger hook.
+
+Only one instance may be enabled at a given time. Use
+L<Devel::Events::Handler::Multiplex> to deliver events to multiple handlers.
+
+=head1 METHODS
+
+=over 4
+
+=item enable
+
+Enable this generator instance, disabling any other instance of
+L<Devel::Events::Generator::LineTrace>.
+
+=item disable
+
+Stop firing events.
+
+=item line
+
+Called by C<DB::DB>. Used to generate the event.
+
+=back
+
+=head1 CAVEATS
+
+Apparently this must be run under C<perl -d>. This is very strange, since
+L<Devel::Events::Generator::SubTrace> doesn't need the C<-d> flag set.
+
+=head1 SEE ALSO
+
+L<perldebguts>, L<Devel::LineTrace>, L<DB>, L<Devel::ebug>, L<perl5db.pl>
+
+=cut
