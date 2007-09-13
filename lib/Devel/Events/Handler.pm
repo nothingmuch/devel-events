e550400a4bf1970c4b82ada14847e4a5642b9f15
Yuval Kogman <nothingmuch@woobling.org>
2007-09-13 00:03:04 +0000

    doc Devel::Events::Handler

--- lib/Devel/Events/Handler.pm
+++ lib/Devel/Events/Handler.pm
@@ -8,3 +8,41 @@ requires "new_event";
 __PACKAGE__;
 
 __END__
+
+=pod
+
+=head1 NAME
+
+Devel::Events::Handler - An optional base role for event handlers.
+
+=head1 SYNOPSIS
+
+	package MyGen;
+	use Moose;
+
+	with qw/Devel::Events::Handler/;
+
+	sub new_event {
+		my ( $self, $type, %data ) = @_;
+
+		# ...
+	}
+
+
+=head1 DESCRIPTION
+
+This convenience role reminds you to add a C<new_event> method.
+
+=head1 REQUIRED METHODS
+
+=over 4
+
+=item new_event @event
+
+Handle a fired event.
+
+=back
+
+=cut
+
+
