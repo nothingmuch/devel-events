900fb44733f8e83ba360a0c9f14c69f51f0afdb1
Yuval Kogman <nothingmuch@woobling.org>
2007-09-13 00:01:52 +0000

    doc Devel::Events::Generator

--- lib/Devel/Events/Generator.pm
+++ lib/Devel/Events/Generator.pm
@@ -19,3 +19,53 @@ __PACKAGE__;
 
 __END__
 
+=pod
+
+=head1 NAME
+
+Devel::Events::Generator - An optional base role for event generators.
+
+=head1 SYNOPSIS
+
+	package MyGen;
+	use Moose;
+
+	with qw/Devel::Events::Generator/;
+
+	sub whatever {
+		my ( $self, @args ) = @_;
+
+		# ...
+
+		$self->send_event( @event );
+	}
+
+=head1 DESCRIPTION
+
+This convenience role provides a basic C<send_event> method, useful for
+implementing generators.
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item handler
+
+Accepts any object.
+
+Required.
+
+=back
+
+=head1 METHODS
+
+=over 4
+
+=item send_event @event
+
+Delegates to C<handler>, calling the method C<new_event> on it.
+
+=back
+
+=cut
+
