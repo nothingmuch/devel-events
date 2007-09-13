3edeb42cff5e48dd0b36171894b5ad30b73e1513
Yuval Kogman <nothingmuch@woobling.org>
2007-09-13 00:11:49 +0000

    docs for Multiplex

--- lib/Devel/Events/Handler/Multiplex.pm
+++ lib/Devel/Events/Handler/Multiplex.pm
@@ -53,3 +53,55 @@ __PACKAGE__;
 
 __END__
 
+=pod
+
+=head1 NAME
+
+Devel::Events::Handler::Multiplex - Delegate events to multiple handlers
+
+=head1 SYNOPSIS
+
+	use Devel::Events::Handler::Multiplex;
+
+	my $h = Devel::Events::Handler::Multiplex->new(
+		handlers => \@handlers,
+	);
+
+	$h->add_handler( $other_handler );
+
+	$h->new_event( ... );
+
+	$h->remove_handler( $some_handler );
+
+=head1 DESCRIPTION
+
+This handler repeats events to any number of sub handlers.
+
+It is useful as a central hub, delegating to any number of sub listeners, from
+any number of generators.
+
+=head1 METHODS
+
+=over 4
+
+=item new_event @handlers
+
+Delegates the event to every one of the sub handlers.
+
+=item handlers
+
+Lists the handlers
+
+=item add_handler $handler
+
+Add a handler to the set of registered handlers.
+
+=item remove_handler $handler
+
+Remove a handler from the set of registered handlers.
+
+=back
+
+=cut
+
+
